e3a7e773f2e78e4a392b746ef0be2e31369f97dc
Tugbay Atilla <tugbayatilla@gmail.com>
2025-01-29 17:00:34 +0100

    refactor: remove Logged decorator from exports

--- lib/core/ally_ai_core/Decorators/__init__.py
+++ lib/core/ally_ai_core/Decorators/__init__.py
@@ -1,6 +1,5 @@
 from .trace import trace, Tracer, default_tracer, default_logger
-from .logged import logged, Logged
 from .timed import timed
 
 
-__all__ = ["logged", "timed", "Logged", trace, Tracer, default_tracer, default_logger]
+__all__ = ["timed", "trace", "Tracer", "default_tracer", "default_logger"]
